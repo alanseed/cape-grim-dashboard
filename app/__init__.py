800ade7cf32a31d067ac80f3e70b6fe69a1df16f
alan.seed <alan.seed@gmail.com>
2021-10-26 14:31:08 +1100

    db name change

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'cg-user.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'cg-dashboard.sqlite'),
     )
 
     if test_config is None:
