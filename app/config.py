9f34dbb07a40588f62e1e3f16aabc35cd2835b80
alan.seed <alan.seed@gmail.com>
2021-11-29 13:17:35 +1100

    update config file

--- app/config.py
+++ app/config.py
@@ -1,10 +1,8 @@
 from os import environ, path
 from dotenv import load_dotenv
 
-
 basedir = path.abspath(path.dirname(__file__))
 env_name = path.join(basedir, '.env') 
-print(env_name)
 load_dotenv(env_name)
 DEBUG=True
 FLASK_ENV='development'
@@ -14,4 +12,3 @@ USERNAME=environ.get('USERNAME')
 PASSWORD=environ.get('PASSWORD') 
 MONGO_USER=environ.get('MONGO_USER')
 MONGO_PASS=environ.get('MONGO_PASS')
-print(f" Secret key = {SECRET_KEY}") 
