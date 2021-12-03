65446f00c56b7db769c86b1b4930369e171ec1ef
alan.seed <alan.seed@gmail.com>
2021-12-03 14:23:30 +1100

    wip main page

--- app/main/main.py
+++ app/main/main.py
@@ -1,20 +1,27 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
 )
-from app.db import get_db 
-from app.chart import make_chart
-import plotly 
-import json 
-import datetime
 
 bp = Blueprint('main', __name__, url_prefix='/main')
 
-# Display the main page to the dashboard 
-@bp.route('/index', methods=('GET', 'POST'))
-def index():
-    chart_name = "ws-comparison" 
-    start = datetime.datetime.fromisoformat("2021-01-01T00:00:00") 
-    end = datetime.datetime.fromisoformat("2021-01-03T00:00:00") 
-    fig = make_chart(chart_name, start, end)
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder) 
+# # Display the main page to the dashboard 
+# @bp.route('/index', methods=('GET', 'POST'))
+# def index():
+#     chart_name = "ws-comparison" 
+#     # start = datetime.datetime.fromisoformat("2021-01-01T00:00:00") 
+#     # end = datetime.datetime.fromisoformat("2021-01-03T00:00:00") 
+#     # fig = make_chart(chart_name, start, end) 
+#     # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder) 
+#     file_name = "/home/awseed/src/cape-grim-dashboard/charts/" + chart_name + ".json" 
+#     with open(file_name) as f:
+#         graphJSON = f.read() 
+#     return render_template('main/index.html', graphJSON = graphJSON, chart_name=chart_name)
+
+@bp.route('/')
+def get_chart():
+    chart_name = request.args.get("chart_name")
+    file_name = "/home/awseed/src/cape-grim-dashboard/charts/" + chart_name + ".json" 
+    with open(file_name) as f:
+        graphJSON = f.read() 
     return render_template('main/index.html', graphJSON = graphJSON)
+    
